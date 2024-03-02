febd88dedcff339f70c8c7de840ab0d542d61062
MuhammadFahadgithub <159857192+MuhammadFahadgithub@users.noreply.github.com>
2024-03-02 13:51:09 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,17 @@
+// Project no.25
+//  Create a variable called alien_color and assign it a value of 'green', 'yellow", or 'red'.
+let alien_color: string= 'green';
+
+// Write an if statment to test whether the alien's color is green. If it is,
+//  print a message that the player just 5 points.
+if(alien_color === 'green'){
+    console.log("Player Just earned 5 points !");
+} 
+// Write one version of this program that passes the if test and another that fails.
+// (The version that fails will have no output.)
+
+alien_color = 'Yellow';
+// (the version that fails will have no output.)
+if (alien_color ==='green'){
+    console.log(" Player Just earned 5 points !");
+}
